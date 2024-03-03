7bb560ac7a1508db8d03c5bdf206e41773f67e56
Scott Harvey <Scott Harvey@harvey41574@gmail.com>
2024-03-02 20:07:06 -0500

    block15

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 //Prompt user to place froyo order, seperating flavors by commas
-const froyoOrder = prompt("Place your froyo order, seperate flavors by commas"
+const froyoInput = prompt("Place your froyo order, seperate flavors by commas"
 );
-
-
-//Split the order into an array of strings.
-
-const froyoArray = froyoOrder.split(",");
+const froyoFlavors = froyoInput.split(",");
+console.log(froyoFlavors);
 
 
 //Create a function to track the flavors
 
-function countFlavors(array) {
-  const froyoObject = []
-  for(let i = 0;i < array.length;i++);
+function countFlavors(flavor) {
+  const froyoObject = {};
+  for(let i = 0; i < flavor.length; i++) {
 //If index i does not exist in the Object, create that property & give a value of 1.
-   if(froyoObject[array[i]] ===undefined) {
-    froyoObject[array[i]] =1;
-   } else {
+   if(froyoObject[flavor[i]] ===undefined) {
+    froyoObject[flavor[i]] =1;
+  } else {
 //If i DOES exist increment the value of that property
-    froyoObject[array[i]] += 1;
-}
+   froyoObject[flavor[i]] += 1;
+   }
+  }
+  console.log(froyoObject);
+
 }
 //return the object(froyoObject);
-console.table(froyoObject);
-countFlavors(array);
+
+countFlavors(froyoFlavors);
 
 
 
